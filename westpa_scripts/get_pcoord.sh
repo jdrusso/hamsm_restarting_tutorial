76324e4c71e535f347f4a67c1142ee88da56370b
John Russo <russojd@ohsu.edu>
2021-07-22 17:12:27 -0700

    Changed get_pcoord to use a temp dir to fix race conditions when using multiple basis states

--- westpa_scripts/get_pcoord.sh
+++ westpa_scripts/get_pcoord.sh
@@ -8,17 +8,25 @@ fi
 cd $WEST_SIM_ROOT
 echo 'I am here'
 #cp ntl9.rst7 parent.rst7
-echo "west data ref: $WEST_STRUCT_DATA_REF" > wsdata.txt
-cp $WEST_STRUCT_DATA_REF ./parent.rst7
+
+WORK_DIR=`mktemp -d`
+
+echo "west data ref: $WEST_STRUCT_DATA_REF" > $WORK_DIR/wsdata.txt
+cp $WEST_STRUCT_DATA_REF $WORK_DIR/parent.rst7
 function cleanup() {
-    rm -f parent.rst7
+    cd $WEST_SIM_ROOT
+    rm -f $WORK_DIR/parent.rst7
    #rm -f phi.dat psi.dat
 }
 
 trap cleanup EXIT
 
 # Get progress coordinate
-cpptraj ref_files/ntl9.prmtop < ref_files/ptraj_init.in || exit 1
+cd $WORK_DIR
+mkdir ref_files
+cp $WEST_SIM_ROOT/ref_files/reference.pdb ref_files/reference.pdb
+
+cpptraj $WEST_SIM_ROOT/ref_files/ntl9.prmtop < $WEST_SIM_ROOT/ref_files/ptraj_init.in || exit 1
 gawk '{print $2}' rmsd.temp | tail -1 > pcoord.dat || exit 1
 cat pcoord.dat > $WEST_PCOORD_RETURN
 #rm -f rmsd.temp
