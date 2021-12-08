008857ba3e6389a8fc4b1311bc43b24e794081c8
John Russo <russojd@.ohsu.edu>
2021-12-08 09:13:45 -0800

    Remove unsupported shell script

--- reset.sh
+++
@@ -1,5 +0,0 @@
-cp restart_initialization.json{.BAK,}
-echo '{"restarts_completed": 0, "runs_completed": 2}' > restart.dat
-cp restart0/run3/west.h5 west.h5
-
-rm pcoord.log pcoord.dat rmsd.temp wsdata.txt
